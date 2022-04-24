02359586bbd3e9c0674f88c94994a974112fded5
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-04-24 19:25:54 -0400

    Convert to md instead of html

--- ProtonDB_Analysis/convert.py
+++ ProtonDB_Analysis/convert.py
@@ -1,54 +1,70 @@
 front_matter_str = """---
 layout: default
-title: 123123
+title: ProtonDB Analysis
 parent: Data Analysis
 grand_parent: Projects
 nav_order: 1
 ---"""
 
 import subprocess
+import shutil
+import os
+
+# function that will prepend given string to given filename
+def prepend_string(filename, string):
+    with open(filename, "r+") as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(string.rstrip("\r\n") + "\n" + content)
+
+
+def move_files(filename):
+    """
+    this function will move all files in source directory to correct path for jekyll
+
+    Args:
+        filename: source ipynb file
+    """
+    if not os.path.exists("iframe_figures"):
+        return
+    filename = os.path.splitext(filename)[0]
+    os.makedirs(filename + "/" + "iframe_figures", exist_ok=True)
+    shutil.copytree(
+        "iframe_figures", filename + "/" + "iframe_figures", dirs_exist_ok=True
+    )
+    shutil.rmtree("iframe_figures")
 
 
 def conv_nb_jekyll(filename, front_matter):
 
     """
-    this function will convert your jupyter notebook to html and
-    prepend the front matter string you provide to the top of the resulting html file
+    this function will convert your jupyter notebook to md and
+    prepend the front matter string you provide to the top of the resulting md file
 
     Args:
         filename: filename of input jupyter notebook (.ipynb file)
         front_matter: python formatted string resembling YAML jekyll front matter
-    Returns:
-        jekyll_html_post: location of final html file to post on your jekyll blog
     """
 
-    # convert jupyter notebook to html
+    # convert jupyter notebook to md
     subprocess.call(
         [
             "jupyter",
             "nbconvert",
             "--to",
-            "html",
-            "--template",
-            "full",
+            "markdown",
             "--no-input",
             filename,
         ]
     )
 
-    # function that will prepend given string to given filename
-    def prepend_string(filename, string):
-        with open(filename, "r+") as f:
-            content = f.read()
-            f.seek(0, 0)
-            f.write(string.rstrip("\r\n") + "\n" + content)
-
     # call function to prepend front matter to the file
-    html_file = filename.replace(".ipynb", ".html")
-    prepend_string(html_file, front_matter)
-    print("converted html file at: ", html_file)
+    md_file = filename.replace(".ipynb", ".md")
+    prepend_string(md_file, front_matter)
 
-    return html_file
+    move_files(filename)
+
+    return md_file
 
 
 jekyll_html_post = conv_nb_jekyll(
